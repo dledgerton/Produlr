879f7736a564f126d539c2d793e6d87299da09be
David Edgerton Jr <david@celluworks.com>
2016-05-31 22:19:36 -0500

    added more functionality to the app

--- _config.py
+++ _config.py
@@ -3,7 +3,7 @@ import os
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DATABASE = 'produlr.db'
+DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
